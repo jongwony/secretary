9781dec091507672d0c9357d497a9ecff7f24034
jongwony <lastone9182@gmail.com>
2020-03-29 16:13:40 +0900

    command sample

--- lambda_function/command_poll.py
+++ lambda_function/command_poll.py
@@ -1,11 +1,5 @@
-import json
-
-import boto3
-import jmespath
-from sqlalchemy.exc import IntegrityError
-
-from lib.connector import rdb_connector
 from urllib.parse import parse_qs
+
 from lib.slack import Slack
 
 
@@ -14,14 +8,33 @@ def lambda_handler(event, context):
     https://api.slack.com/interactivity/slash-commands
     """
     print(f'{event=}')
-    parse_qs(event['body'])
-    Slack.server_response(200, body={
+    body = parse_qs(event['body'])
+    text = body['text']
+    print(body)
+
+    payload = {
         "response_type": "in_channel",
-        "text": "It's 80 degrees right now."
-    })
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "*It's 80 degrees right now.*"
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Partly cloudy today and tomorrow"
+                }
+            }
+        ]
+    }
     """
     {
       "response_type": "ephemeral",
       "text": "Sorry, that didn't work. Please try again."
     }
     """
+    return Slack.server_response(200, body=payload)
