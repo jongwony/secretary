122fc5e3652f5ecd5082aa41395574bc7322809e
jongwony <lastone9182@gmail.com>
2020-06-01 01:07:41 +0900

    dynamic dynamodb keyvalue store

--- lambda_function/event_message_changed.py
+++ lambda_function/event_message_changed.py
@@ -12,25 +12,24 @@ def nosql_body_dump(body):
     return nosql_table.put_item(Item=body)
 
 
-def integrity_check(client_msg_id, url, title, user, channel):
+def integrity_check(url, channel, **kwargs):
     def post_slack():
         bot = Slack()
         bot.post_message(text=f'중복 url: {url}', channel=channel, username='Link Crawler')
 
     data = {
         'id': url,
-        'title': title,
-        'user': user,
         'channel': channel,
-        'client_msg_id': client_msg_id,
+        **kwargs
     }
     dynamodb = boto3.resource('dynamodb', region_name='ap-northeast-2')
     nosql_table = dynamodb.Table('secretary')
 
     try:
-        response = nosql_table.get_item(Key=data)
-        assert response.get('Item') is not None
-    except AssertionError:
+        response = nosql_table.get_item(Key={'id': url})
+        assert response.get('Item') is None
+    except AssertionError as e:
+        print(e, url)
         post_slack()
     else:
         nosql_body_dump(data)
@@ -40,17 +39,17 @@ def lambda_handler(event, context):
     body = json.loads(event.pop('body'))
 
     channel = jmespath.search('event.channel', body)
-    user, client_msg_id = jmespath.search('event | message.[user, client_msg_id] || [user, client_msg_id]', body)
-    attachments = jmespath.search('event.message.attachments[].[title_link, title]', body) or []
+    user, origin_text = jmespath.search('event | message.[user, text] || [user, text]', body)
+    attachments = jmespath.search('event.message.attachments[].[title_link, title, text]', body) or []
     blocks = jmespath.search('event.message.blocks[] || event.blocks[]', body)
     urls = jmespath.search('[].elements[].elements[].url', blocks) or []
 
     if not attachments:
         return
 
-    for url, title in attachments:
-        integrity_check(client_msg_id, url, title, user, channel)
+    for url, title, text in attachments:
+        integrity_check(url, channel, user=user, title=title, text=text, origin_text=origin_text)
 
     if diff_set := set(urls) - set(x[0] for x in attachments):
         for url in diff_set:
-            integrity_check(client_msg_id, url, None, user, channel)
+            integrity_check(url, channel, user=user, origin_text=origin_text)
